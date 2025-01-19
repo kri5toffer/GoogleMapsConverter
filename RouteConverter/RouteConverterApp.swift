751f3c304b9dde748a9f8eaec5ac3744085c30c5
Christopher Siang <chris.siang123@gmail.com>
2025-01-19 16:39:34 +1100

    Initial Commit

---
+++ RouteConverter/RouteConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteConverterApp.swift
+//  RouteConverter
+//
+//  Created by Christopher Siang on 19/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct RouteConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
